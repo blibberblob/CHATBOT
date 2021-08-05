d4f8682dbdda3726cbe810c3b1e0e6fa500bd265
issy <ibrehmer@gmail.com>
2021-08-05 19:17:52 +0100

    cleanup

--- bot.py
+++ bot.py
@@ -1,20 +1,15 @@
 import random
-import pyttsx3
-import speech_recognition as sr #convert speech to text
-import datetime #for fetching date and time
-import wolframalpha # to calculate strings into formula
+import pyttsx3  # convert text to speech
+import speech_recognition as sr  # convert speech to text
+import datetime  # for fetching date and time
+import wolframalpha  # to calculate strings into formula
 
-
-# Initialize the converter
+# Initialize text to speech
 converter = pyttsx3.init()
-# Set properties before adding
-# Things to say
-# Sets speed percent
-# Can be more than 100
 converter.setProperty('rate', 150)
-# Set volume 0-1
 converter.setProperty('volume', 0.7)
 
+# List of jokes
 jokes = ["What kind of tea is hard to swallow? reality !",
          "How does a train eat? it goes chew chew ",
          "Why can't you hear a Pterodactyl going to the bathroom? Because the P is silent.",
@@ -23,7 +18,9 @@ jokes = ["What kind of tea is hard to swallow? reality !",
          "whats grey and cant fly? a parking lot",
          "Where do mermaids look for jobs? The kelp-wanted section"]
 
-def talk():
+
+# Function to listen to mic and return text
+def listen():
     input = sr.Recognizer()
     with sr.Microphone() as source:
         audio = input.listen(source)
@@ -35,21 +32,22 @@ def talk():
         except sr.UnknownValueError:
             print("Sorry I did not hear your question, Please repeat again.")
 
-
     return data
 
+
+# Function to read out the text(output)
 def respond(output):
     converter.say(output)
     converter.runAndWait()
 
 
-
+# Main Program
 if __name__ == '__main__':
     respond("Hi, I am Chatbot your personal assistant")
 
-    while (1):
+    while 1:
         respond("How can I help you?")
-        text = talk().lower()
+        text = listen().lower()
 
         if text == 0:
             continue
@@ -62,7 +60,7 @@ if __name__ == '__main__':
             random_joke = random.choice(jokes)
             respond(random_joke)
 
-        elif 'weather' in text:
+        elif 'weather' or 'umbrella' in text:
             respond("Just look out of the window!")
 
         elif 'time' in text:
@@ -73,14 +71,13 @@ if __name__ == '__main__':
             strDate = datetime.datetime.now().strftime("%d %b")
             respond(f"the date is {strDate}")
 
-        #elif "calculate" or "what is" in text:
-            #question = talk()
-            #app_id = "678E4V-X9U4UUVR85"
-            #client = wolframalpha.Client(app_id)
-            #res = client.query(text)
-            #answer = next(res.results).text
-            #respond("The answer is " + answer)
+        # elif "calculate" or "what is" in text:
+        # question = listen()
+        # app_id = "678E4V-X9U4UUVR85"
+        # client = wolframalpha.Client(app_id)
+        # res = client.query(text)
+        # answer = next(res.results).text
+        # respond("The answer is " + answer)
 
         else:
             respond("I'm not sure what to do")
-
