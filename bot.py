758fcb491b99a067ed3fa0614e350549a7bc6eca
blibberblob <88207837+blibberblob@users.noreply.github.com>
2021-08-23 16:28:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ def respond(output):
 
 # Main Program
 if __name__ == '__main__':
-    respond("Hi, I am Bob Something your personal assistant")
+    respond("Hi, I am Bob your personal assistant")
 
     while 1:
         respond("How can I help you?")
